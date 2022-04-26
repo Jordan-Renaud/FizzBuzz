86d8aa3c4fb05a1074abeb408303a607fae0448d
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-26 11:57:35 +0100

    add fixes

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -11,31 +11,25 @@ greet();
 // 3. print “Buzz” whenever the number is a multiple of 5, instead of the number itself.
 //      Also, make it that “FizzBuzz” is printed whenever the number is a multiple of 3 and 5.
 
-function FizzBuzzNeeded(currentNum) {
-  return currentNum % 3 == 0 || currentNum % 5 == 0;
+function fizzBuzzNeeded(currentNum) {
+  return currentNum % 3 === 0 || currentNum % 5 === 0;
 }
 
-function setFizzBuzz(currentNum) {
+function getFizzBuzz(currentNum) {
   let phrase = "";
 
-  if (currentNum % 3 == 0) phrase += "Fizz";
-  if (currentNum % 5 == 0) phrase += "Buzz";
+  if (currentNum % 3 === 0) phrase += "Fizz";
+  if (currentNum % 5 === 0) phrase += "Buzz";
 
   return phrase;
 }
 
 for (let i = 1; i <= 100; i++) {
-  let phrase = "";
-
-  if (FizzBuzzNeeded(i)) {
-    phrase = setFizzBuzz(i);
+  if (fizzBuzzNeeded(i)) {
+    console.log(getFizzBuzz(i));
   } else {
-    phrase = `${i}`;
+    console.log(i.toString());
   }
-
-  console.log(phrase);
 }
 
 // Questions
-// == vs === preferences? - currently easier to read with ==
-// readability with line 33?
