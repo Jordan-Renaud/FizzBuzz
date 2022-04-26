80bc4bc022596adb90de599ac033e8f460645eba
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-26 11:33:00 +0100

    pull out FizzBuzz into function

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -11,23 +11,31 @@ greet();
 // 3. print “Buzz” whenever the number is a multiple of 5, instead of the number itself.
 //      Also, make it that “FizzBuzz” is printed whenever the number is a multiple of 3 and 5.
 
-for (let currentNum = 1; currentNum <= 100; currentNum++) {
+function FizzBuzzNeeded(currentNum) {
+  return currentNum % 3 == 0 || currentNum % 5 == 0;
+}
+
+function setFizzBuzz(currentNum) {
   let phrase = "";
 
-  //set phrase to follow rules
-  if (currentNum % 3 == 0 && currentNum % 5 == 0) {
-    phrase = "FizzBuzz";
-  } else if (currentNum % 3 == 0) {
-    phrase = "Fizz";
-  } else if (currentNum % 5 == 0) {
-    phrase = "Buzz";
+  if (currentNum % 3 == 0) phrase += "Fizz";
+  if (currentNum % 5 == 0) phrase += "Buzz";
+
+  return phrase;
+}
+
+for (let i = 1; i <= 100; i++) {
+  let phrase = "";
+
+  if (FizzBuzzNeeded(i)) {
+    phrase = setFizzBuzz(i);
   } else {
-    phrase = `${currentNum}`;
+    phrase = `${i}`;
   }
 
   console.log(phrase);
 }
 
 // Questions
-// i readability vs currentNum - read like english or follow javascript convention
 // == vs === preferences? - currently easier to read with ==
+// readability with line 33?
