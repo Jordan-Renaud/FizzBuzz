7af82a91475bd5e87ebeca1ff5a1d43d9aec1b4c
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-26 10:37:06 +0100

    add hello world

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,26 +3,3 @@ function greet() {
 }
 
 greet();
-
-for (let index = 1; index <= 100; index++) {
-  let phrase = "";
-
-  //set phrase
-  if (index % 3 == 0 && index % 5 == 0) {
-    phrase = "FizzBuzz";
-  } else if (index % 3 == 0) {
-    phrase = "Fizz";
-  } else if (index % 5 == 0) {
-    phrase = "Buzz";
-  } else if (index % 11 == 0) {
-    phrase = "Bong";
-  } else {
-    phrase = `${index}`;
-  }
-
-  if (index % 7 == 0) {
-    phrase += "Bang";
-  }
-
-  console.log(phrase);
-}
